2189ffba18886eafa1f6a56db3fcfd3b0b1fcbe3
Ed @ PEI <ed.henderson@peigenesis.com>
2021-01-30 17:33:10 -0700

    Moved repo to github

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A basic, random team generator for tennis groups",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/sharpertool/teamgen",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
